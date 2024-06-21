28b1acb79204cc32cc915e06c435ae3ca4381c30
blakeminix <blakeminix2@gmail.com>
2024-06-21 15:51:18 -0400

    Cost optimization

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import uuid
 import threading
 import json
 import requests
+import tensorflow as tf
+from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
@@ -25,6 +27,22 @@ s3_client = boto3.client(
     aws_secret_access_key=SECRET_ACCESS_KEY,
 )
 
+model = tf.keras.models.load_model('image_recognition_model_cifar100.h5')
+
+# Model's labels
+labels = [
+    "apple", "aquarium_fish", "baby", "bear", "beaver", "bed", "bee", "beetle", "bicycle", "bottle",
+    "bowl", "boy", "bridge", "bus", "butterfly", "camel", "can", "castle", "caterpillar", "cattle",
+    "chair", "chimpanzee", "clock", "cloud", "cockroach", "couch", "crab", "crocodile", "cup", "dinosaur",
+    "dolphin", "elephant", "flatfish", "forest", "fox", "girl", "hamster", "house", "kangaroo", "keyboard",
+    "lamp", "lawn_mower", "leopard", "lion", "lizard", "lobster", "man", "maple_tree", "motorcycle", "mountain",
+    "mouse", "mushroom", "oak_tree", "orange", "orchid", "otter", "palm_tree", "pear", "pickup_truck", "pine_tree",
+    "plain", "plate", "poppy", "porcupine", "possum", "rabbit", "raccoon", "ray", "road", "rocket",
+    "rose", "sea", "seal", "shark", "shrew", "skunk", "skyscraper", "snail", "snake", "spider",
+    "squirrel", "streetcar", "sunflower", "sweet_pepper", "table", "tank", "telephone", "television", "tiger", "tractor",
+    "train", "trout", "tulip", "turtle", "wardrobe", "whale", "willow_tree", "wolf", "woman", "worm"
+]
+
 def initiate_processing(filename):
     try:
         print(f'Initiating processing for {filename}')
@@ -42,6 +60,54 @@ def initiate_processing(filename):
         print(f"Error initiating processing: {e}")
 
 
+def process_image(filename):
+    try:
+        local_filename = filename
+        s3_client.download_file(S3_BUCKET, filename, local_filename)
+        print('File downloaded locally.')
+
+        img = Image.open(local_filename)
+        if img.mode == 'RGBA':
+            img = img.convert('RGB')
+        img = img.resize((32, 32))
+        img_array = tf.keras.preprocessing.image.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)
+        img_array /= 255.0
+
+        # Predict
+        prediction = model.predict(img_array)
+        predicted_index = tf.argmax(prediction[0]).numpy()
+        predicted_label = labels[predicted_index]
+
+        result = {
+            'prediction': prediction.tolist(),
+            'predicted_label': predicted_label
+        }
+
+        result_filename = f'{filename}.json'
+        s3_client.put_object(Bucket=S3_BUCKET, Key=result_filename, Body=json.dumps(result))
+        print(f"Prediction stored in S3: {result_filename}")
+
+    except Exception as e:
+        print(f"Error processing image: {e}")
+        error_result = {'error': 'Error processing image'}
+        result_filename = f'{filename}.json'
+        s3_client.put_object(Bucket=S3_BUCKET, Key=result_filename, Body=json.dumps(error_result))
+
+    finally:
+        try:
+            os.remove(local_filename)
+            print(f"Deleted local file: {local_filename}")
+        except Exception as e:
+            print(f"Error deleting file: {e}")
+
+        try:
+            s3_client.delete_object(Bucket=S3_BUCKET, Key=filename)
+            print(f"Deleted file from S3: {filename}")
+        except Exception as e:
+            print(f"Error deleting file from S3: {e}")
+
+
 @app.route('/api/upload', methods=['POST'])
 def upload_image():
     if 'file' not in request.files:
@@ -57,7 +123,8 @@ def upload_image():
     try:
         s3_client.upload_fileobj(file, S3_BUCKET, filename)
         print('File uploaded to S3.')
-        threading.Thread(target=initiate_processing, args=(filename,)).start()
+        # threading.Thread(target=initiate_processing, args=(filename,)).start()
+        threading.Thread(target=process_image, args=(filename,)).start()
         return jsonify({'message': 'File uploaded and processing initiated', 'filename': filename}), 200
     except Exception as e:
         print(f"Error uploading file to S3: {e}")
